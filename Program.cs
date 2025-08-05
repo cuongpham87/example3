a48acebe674c8a6ae6d11c91b7dddcb96dc3d529
cuongpham87 <lil.steven296@gmail.com>
2025-08-05 21:49:56 +0630

    add
    add

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("xin chào các bạn!");
+        Console.Write("giới thiệu bản thân!");
+        Console.Write("Tôi khá đẹp trai");
+    }
+}
