2288674639698c1fa86e9c7f4719100e6819dd71
cuongpham87 <lil.steven296@gmail.com>
2025-08-05 22:35:07 +0630

    add
    add

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿//in ra màn hình yêu cầu nhập một ký tự từ bàn phím
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Numerics;
 
 public class Program
 {
@@ -13,6 +14,13 @@ public class Program
         Console.WriteLine("nhập một số trên bàn phím");
         int so = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("" + so);
+        //tính tổng số nguyên thứ nhất và số nguyên thứ hai 
+        Console.WriteLine("số nguyên thứ nhất");
+        int so1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("số nguyên thứ hai");
+        int so2 = Convert.ToInt32(Console.ReadLine());
+        int tong = so1 + so2;
+        Console.WriteLine($"Tổng là {tong}");
 
     }
 }
