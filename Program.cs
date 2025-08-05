3904385e70592632d7ead7fbdc625fcb35541b53
cuongpham87 <lil.steven296@gmail.com>
2025-08-05 22:18:34 +0630

    add
    add

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
-﻿public class Program
+﻿//in ra màn hình yêu cầu nhập một ký tự từ bàn phím
+using System.ComponentModel;
+using System.Diagnostics;
+
+public class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("xin chào các bạn!");
         Console.Write("giới thiệu bản thân!");
         Console.Write("Tôi khá đẹp trai");
+        //in ra màn hình yêu cầu nhập một ký tự từ bàn phím, chọn 1 số rồi in ra màn hình
+        Console.WriteLine("nhập một số trên bàn phím");
+        int so = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("" + so);
+
     }
 }
